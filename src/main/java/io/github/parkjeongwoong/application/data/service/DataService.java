3674073228919ec45ea19b7431b19ce8859e7c3d
dvlprjw <dvlprjw@gmail.com>
2023-01-20 16:04:25 +0900

    Sub Server Setup v0.2.7 - backup test

--- src/main/java/io/github/parkjeongwoong/application/data/service/DataService.java
+++ src/main/java/io/github/parkjeongwoong/application/data/service/DataService.java
@@ -47,10 +47,10 @@ public class DataService implements DataUsecase {
 
     @Override
     public boolean backup() throws IOException, InterruptedException {
-//        String command = String.format("mysqldump -u %s -p %s --add-drop-table --databases %s -r %s",
+//        String command = String.format("/usr/bin/mysqldump -u %s -p %s --add-drop-table --databases %s -r %s",
 //                dbUsername, dbPassword, dbName, outputFile);
 //        Process process = Runtime.getRuntime().exec(command);
-        String command = String.format("mysqldump -u %s -p %s > %s",
+        String command = String.format("/usr/bin/mysqldump -u %s -p %s > %s",
                 dbUsername, dbName, outputFile);
         String[] commandArray = new String[] {command, dbPassword};
         Process process = Runtime.getRuntime().exec(commandArray);
