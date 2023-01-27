acc1453a281379e8e7d46e5b3c921dbff443ead6
dvlprjw <dvlprjw@gmail.com>
2023-01-27 23:17:51 +0900

    Sub Server Setup v0.4 - db restore test, sync test

--- src/main/java/io/github/parkjeongwoong/application/data/dto/SyncServerRequestDto.java
+++ src/main/java/io/github/parkjeongwoong/application/data/dto/SyncServerRequestDto.java
@@ -1,14 +1,11 @@
 package io.github.parkjeongwoong.application.data.dto;
 
 import lombok.Builder;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
+@Getter
 public class SyncServerRequestDto {
     private final String password;
 
